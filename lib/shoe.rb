a6369268dae1ad42fd4eebc1818da789c7b34189
Justin Phillips <justinphil02@yahoo.com>
2019-10-23 18:59:09 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
 # Make your shoe class here!
+class Shoe
+def initialize(brand)
+  @brand = brand
+end
+attr_writer :brand, :color, :size, :material, :condition
+attr_reader :brand, :color, :size, :material, :condition
+#def condition
+#  @condition
+#end
+#def condition=(status)
+#  @condition = status
+#end
+
+
+def cobble
+  @condition = "new"
+  puts "Your shoe is as good as new!"
+end
+
+
+
+end
